a7d1cd085c3a1ec86479ead52dafc759edf1cd02
vamsi krishna <saivamsikrishna.2023@gmail.com>
2025-10-28 12:53:39 +0530

    Add files via upload

---
+++ 2.Find the area of circle.cpp
@@ -0,0 +1,17 @@
+//Find the area of circle
+#include <stdio.h> 
+#define PI 3.14
+int main()
+{
+// declare variables float radius, area;
+float radius,area;
+// take input
+printf("Enter Radius of Circle(in cm): ");
+scanf("%f",&radius);
+// calculate area
+area = PI * radius * radius;
+// display result
+printf("Area of Circle = %.2f cm\n", area);
+return 0;
+}
+
